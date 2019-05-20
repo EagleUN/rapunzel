f3b0225f3eda3c71051f44dc610aeb278b6acee8
lsfinite <lbsantosg@unal.edu.co>
2019-05-19 21:20:48 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,45 @@
+import os 
+import datetime 
+import json
+from flask import Flask, redirect, url_for, request, render_template,jsonify 
+from pymongo import MongoClient
+from bson.json_util import dumps
+app= Flask(__name__)
+client= MongoClient(os.environ['DB_PORT_27017_TCP_ADDR'])
+db= client.notifications 
+
+@app.route('/')
+def todo():
+	_items = db.notifications.find()
+	items = [item for item in _items]
+	return render_template('todo.html', items=items)
+
+@app.route('/users/<user_id>/followers/<follower_id>', methods=['POST'])
+def new_follow(user_id,follower_id):
+	item_doc = {
+		'notificated_user': user_id,
+		'follower': follower_id,
+		'date' : datetime.datetime.now(),
+		'type' : "follow"}
+	db.notifications.insert_one(item_doc)
+	return redirect(url_for('todo'))
+
+@app.route('/users/<user_id>/posts/<post_id>/shares/<follower_id>', methods=['POST'])
+def new_share(user_id,post_id,follower_id):
+    item_doc = {
+        'notificated_user': user_id,
+		'post_id' : post_id,
+        'follower': follower_id,
+        'date' : datetime.datetime.now(),
+		'type' : "share"}
+    db.notifications.insert_one(item_doc)
+    return redirect(url_for('todo'))
+
+@app.route('/notifications/<user_id>')
+def get_notifications(user_id):
+    _items = db.notifications.find({"notificated_user": user_id})
+    items = [item for item in _items]
+    return dumps(items)
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', debug=True)
