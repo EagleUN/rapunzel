e124d583befeb7ebb7a574e62cf2bffea4978862
root <root@rancher-node.us-central1-a.c.eagleun.internal>
2019-06-03 13:45:00 +0000

    set starndard user

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ import os
 import datetime 
 import json
 import logging
+import requests 
 from flask import Flask, redirect, url_for, request, render_template,jsonify 
 from pymongo import MongoClient
 from bson.json_util import dumps
+from graphqlclient import GraphQLClient
 app= Flask(__name__)
 client= MongoClient(host=['rapunzel_db:27017'], connect = True)
 db= client.notifications 
+QLclient = GraphQLClient('http://35.232.95.82:5000/graphql')
 
 def myconverter(o):
     if isinstance(o, datetime.datetime):
@@ -31,14 +34,14 @@ def new_follow(user_id,follower_id):
 
 @app.route('/posts/<post_id>/shares/<follower_id>', methods=['POST'])
 def new_share(post_id,follower_id):
-    item_doc = {
-        'notificated_user': "followed_id",
+	item_doc = {
+		'notificated_user': "1daba304-fa24-402c-bee6-8d4860374aed",
 		'post_id' : post_id,
-        'follower': follower_id,
-        'date' : datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"),
+		'follower': follower_id,
+		'date' : datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"),
 		'type' : "share"}
-    db.notifications.insert_one(item_doc)
-    return dumps(item_doc)
+	db.notifications.insert_one(item_doc)
+	return dumps(item_doc)
 
 @app.route('/notifications/<user_id>')
 def get_notifications(user_id):
