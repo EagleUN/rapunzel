d75a2399276825473981afd7a095ff22b7bb7870
Laura Santos <lbsantosg@unal.edu.co>
2019-06-08 17:32:49 -0500

    add info to retrieved data

--- app.py
+++ app.py
@@ -21,12 +21,19 @@ def makeQuery(id, query,params):
 
 @app.route('/test')
 def testquery():
-	post_id = "f7f92678-e978-4e80-9b43-59e0472aa612"
-	post = makeQuery("\""+post_id+"\"","postById","{id createdAt idCreator content}")
-	notificated_id = post["idCreator"]
-	id = "{id:\""+notificated_id+ "\"}"
-	user = makeQuery(id,"userById","{id name last_name email}")
-	return dumps(user)
+	user_id = "777c3dc5-e33c-497a-ab09-a93a38516b6c"
+	_items = db.notifications.find({"notificated_user": user_id})
+	items = [item for item in _items]
+	for i in items: 
+		follower_id = i["follower"]
+		follower = makeQuery("{id:\""+follower_id+ "\"}", "userById","{id name last_name email}")
+		if i["type"] == "follow":
+			i["follower_id"] = follower["name"] + " " + follower["last_name"]
+		else: 
+			post_id = i["post_id"]
+			post = makeQuery("\""+post_id+"\"","postById","{id createdAt idCreator content}")
+			i["content"] = post["content"]
+	return dumps(items)
 	
 @app.route('/notifications')
 def allNotif():
@@ -40,7 +47,7 @@ def new_follow(user_id,follower_id):
 	follower = makeQuery("{id:\""+follower_id+ "\"}", "userById","{id name last_name email}")
 	item_doc = {
 		'notificated_user': user_id,
-		'follower' : follower["name"] + " " + follower["last_name"],
+		'follower' : follower_id,
 		'date' : datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"),
 		'type' : "follow"}
 	db.notifications.insert_one(item_doc)
@@ -49,13 +56,13 @@ def new_follow(user_id,follower_id):
 @app.route('/posts/<post_id>/shares/<follower_id>', methods=['POST'])
 def new_share(post_id,follower_id):
 	post = makeQuery("\""+post_id+"\"","postById","{id createdAt idCreator content}")
-	notificated_id = post["idCreator"]
-	id = "{id:\""+notificated_id+ "\"}"
-	notificated_user = makeQuery(id,"userById","{id name last_name email}")
+	notificated_user = post["idCreator"]
+	id = "{id:\""+follower_id+ "\"}"
+	follower = makeQuery(id,"userById","{id name last_name email}")
 	#app.logger.info(user)
 	item_doc = {
-		'notificated_user': notificated_user["id"],
-		'post_id' : post["id"],
+		'notificated_user': notificated_user,
+		'post_id' : post_id,
 		'follower': follower_id,
 		'date' : datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"),
 		'type' : "share"}
@@ -70,3 +77,6 @@ def get_notifications(user_id):
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port= '5050' , debug=True)
+
+
+#x =  '[{"date": "2019-06-08 19:44:53.894586","notificated_user": "777c3dc5-e33c-497a-ab09-a93a38516b6c","follower": "Juan Diego Moreno ","_id": {"$oid": "5cfc1035080861b195d3a082"},"type": "follow"},{"date": "2019-06-08 19:45:25.368146","notificated_user": "777c3dc5-e33c-497a-ab09-a93a38516b6c","follower": "Juan Sebastian Chaves","_id": {"$oid": "5cfc1055080861b195d3a083"},"type": "follow"},{"post_id": "87e1f6d5-d3f1-4582-b646-44df2ee89c67","follower": "1b2e01c2-327b-4753-abe9-b9d1e20fdb8d","notificated_user": "777c3dc5-e33c-497a-ab09-a93a38516b6c","date": "2019-06-08 19:47:49.455945","_id": {"$oid": "5cfc10e5080861b195d3a084"},"type": "share"},{        "post_id": "2a5aeb28-f94c-42dd-ac92-361b0d3816d9",        "follower": "cb4845dd-0299-4462-9994-32acd5e86736","notificated_user": "777c3dc5-e33c-497a-ab09-a93a38516b6c",    "date": "2019-06-08 19:49:39.343245",        "_id": {            "$oid": "5cfc1153080861b195d3a085"        },        "type": "share"    }]'
