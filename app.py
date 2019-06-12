937ee8610aca9c4718d6c7409f8eee40ec3d2b89
Laura Santos <lbsantosg@unal.edu.co>
2019-06-12 15:51:13 -0500

    add query to find path

--- app.py
+++ app.py
@@ -28,11 +28,12 @@ def testquery():
 		follower_id = i["follower"]
 		follower = makeQuery("{id:\""+follower_id+ "\"}", "userById","{id name last_name email}")
 		if i["type"] == "follow":
-			i["follower_id"] = follower["name"] + " " + follower["last_name"]
+			i["follower_name"] = follower["name"] + " " + follower["last_name"]
 		else: 
 			post_id = i["post_id"]
 			post = makeQuery("\""+post_id+"\"","postById","{id createdAt idCreator content}")
 			i["content"] = post["content"]
+			i["follower_name"] = follower["name"] + " " + follower["last_name"]
 	return dumps(items)
 	
 @app.route('/notifications')
@@ -71,12 +72,20 @@ def new_share(post_id,follower_id):
 
 @app.route('/notifications/<user_id>')
 def get_notifications(user_id):
-    _items = db.notifications.find({"notificated_user": user_id})
-    items = [item for item in _items]
-    return dumps(items)
+	_items = db.notifications.find({"notificated_user": user_id})
+	items = [item for item in _items]
+	for i in items: 
+		follower_id = i["follower"]
+		follower = makeQuery("{id:\""+follower_id+ "\"}", "userById","{id name last_name email}")
+		if i["type"] == "follow":
+			i["follower_name"] = follower["name"] + " " + follower["last_name"]
+		else: 
+			post_id = i["post_id"]
+			post = makeQuery("\""+post_id+"\"","postById","{id createdAt idCreator content}")
+			i["content"] = post["content"]
+			i["follower_name"] = follower["name"] + " " + follower["last_name"]
+	return dumps(items)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port= '5050' , debug=True)
 
-
-#x =  '[{"date": "2019-06-08 19:44:53.894586","notificated_user": "777c3dc5-e33c-497a-ab09-a93a38516b6c","follower": "Juan Diego Moreno ","_id": {"$oid": "5cfc1035080861b195d3a082"},"type": "follow"},{"date": "2019-06-08 19:45:25.368146","notificated_user": "777c3dc5-e33c-497a-ab09-a93a38516b6c","follower": "Juan Sebastian Chaves","_id": {"$oid": "5cfc1055080861b195d3a083"},"type": "follow"},{"post_id": "87e1f6d5-d3f1-4582-b646-44df2ee89c67","follower": "1b2e01c2-327b-4753-abe9-b9d1e20fdb8d","notificated_user": "777c3dc5-e33c-497a-ab09-a93a38516b6c","date": "2019-06-08 19:47:49.455945","_id": {"$oid": "5cfc10e5080861b195d3a084"},"type": "share"},{        "post_id": "2a5aeb28-f94c-42dd-ac92-361b0d3816d9",        "follower": "cb4845dd-0299-4462-9994-32acd5e86736","notificated_user": "777c3dc5-e33c-497a-ab09-a93a38516b6c",    "date": "2019-06-08 19:49:39.343245",        "_id": {            "$oid": "5cfc1153080861b195d3a085"        },        "type": "share"    }]'
